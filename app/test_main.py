b3b1c98e1ed51b37c73e700a190074354deee352
usrofgh <mailforgimail@gmail.com>
2023-02-19 22:04:30 +0200

    solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,21 @@
+import pytest
+
 from app.main import is_isogram
 
-# write your code here
+
+class TestIsIsogramMethod:
+    @pytest.mark.parametrize(
+        "word,expected_result",
+        [
+            ("playgrounds", True),
+            ("look", False),
+            ("Adam", False),
+            ("", True),
+        ]
+    )
+    def test_is_isogram(
+            self,
+            word: str,
+            expected_result: bool
+    ) -> None:
+        assert is_isogram(word) == expected_result
