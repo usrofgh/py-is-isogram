97ff5cdad1801f77d798ffd8321dab67877af156
Vladyslav Shabas <vladyslav.shabas@visahq.com>
2024-05-28 18:55:32 +0300

    solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,24 @@
+import pytest
+
 from app.main import is_isogram
 
-# write your code here
+
+@pytest.mark.parametrize(
+    "word, expected",
+    [
+        pytest.param("playgrounds", True, id="Should pass if word is isogram"),
+        pytest.param(
+            "Adam",
+            False,
+            id="Should pass if exist duplicates with different keyboard layout"
+        ),
+        pytest.param(
+            "look",
+            False,
+            id="Should pass if exist consecutive same letters"
+        ),
+        pytest.param("", True, id="Should pass if word is empty"),
+    ]
+)
+def test_pass(word: str, expected: bool) -> None:
+    assert is_isogram(word) == expected
